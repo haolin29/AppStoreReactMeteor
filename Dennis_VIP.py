96e072e59d6cd0210719ebb96a3ddc63025d9b43
wjcdenis <denniswjc@gmail.com>
2016-06-16 10:47:10 -0400

    Update Dennis_VIP.py

--- Dennis_VIP.py
+++ Dennis_VIP.py
@@ -7,17 +7,13 @@ Created on Sat May 21 19:41:40 2016
 import json
 import numpy as np
 import pandas as pd
+from pandas import DataFrame, Series
         
-appfile_set = set()
-uid_set = set()
-app_set = set()
-acttime_set = set()
-cateid_set = set()
-#设置好各个feature的set
-data = []
-with open('/Users/dennis/Desktop/bigtiger/vip service prediction/user.json') as f:
-    for line in f:
-        data.append(json.loads(line)) 
-        #把json转换成python对象 json,loads
-        N = len(data) #number of data
-        for 
+path = '/Users/dennis/Desktop/bigtiger/vip service prediction/user.json'#设定路径 
+records = [json.loads(line) for line in open(path)] #用for in 把数据导入， json.loads把json变成python字典
+
+frame = DataFrame(records)
+print ("The number of null:")
+frame.isnull().values.sum()#查看有无空值
+
+
