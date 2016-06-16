643c79841ce85d6d67abcfb09cfefec1a0e46b2f
wjcdenis <denniswjc@gmail.com>
2016-06-15 21:59:15 -0400

    Update Dennis_VIP.py

--- Dennis_VIP.py
+++ Dennis_VIP.py
@@ -6,10 +6,18 @@ Created on Sat May 21 19:41:40 2016
 """
 import json
 import numpy as np
-
+import pandas as pd
+        
+appfile_set = set()
+uid_set = set()
+app_set = set()
+acttime_set = set()
+cateid_set = set()
+#设置好各个feature的set
 data = []
 with open('/Users/dennis/Desktop/bigtiger/vip service prediction/user.json') as f:
     for line in f:
         data.append(json.loads(line)) 
         #把json转换成python对象 json,loads
-        
+        N = len(data) #number of data
+        for 
