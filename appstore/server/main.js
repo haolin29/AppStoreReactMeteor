94938099de95c7e0fc529c3557a4b2a10c9f482d
Han <hanyu2@asu.edu>
2016-06-21 17:49:37 -0700

    Fix directory

--- appstore/server/main.js
+++ appstore/server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Apps } from '../imports/collections/apps'
+import { Apps } from '../lib/collections/apps'
 
 Meteor.startup(() => {
   // code to run on server at startup
