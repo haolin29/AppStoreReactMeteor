d74e528368724a4989e575981ef59c16490e39cf
z502185331 <lieyongzou@Lieyongs-MBP.wv.cc.cmu.edu>
2016-06-25 10:45:13 -0400

    Merge

--- Dennis_VIP.py
+++ Dennis_VIP.py
@@ -8,12 +8,9 @@ import json
 import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
-        
-path = '/Users/dennis/Desktop/bigtiger/vip service prediction/user.json'#设定路径 
+
+path = '/Users/dennis/Desktop/bigtiger/vip service prediction/user.json'#设定路径
 records = [json.loads(line) for line in open(path)] #用for in 把数据导入， json.loads把json变成python字典
 
 frame = DataFrame(records)
 N= frame.isnull().values.sum()#查看有无空值
-
-
-
