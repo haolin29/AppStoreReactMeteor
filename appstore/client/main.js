bd3a4ac07c953fa43b96bf1d992d8a349099ea0c
Han <hanyu2@asu.edu>
2016-06-29 17:00:27 -0700

    Merge

--- appstore/client/main.js
+++ appstore/client/main.js
@@ -4,8 +4,4 @@ import ReactDOM from 'react-dom';
 import AppDetail from './components/app_details';
 import AppPage from './components/app_page';*/
 
-<<<<<<< HEAD
 import '/imports/routes';
-=======
-import '/imports/routes';
->>>>>>> fab03d1325671f7cbcf5fc53cb1e680665c8af06
