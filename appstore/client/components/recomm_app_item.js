80a126e97437fd30a3dd6831ef136afca0a97af2
Han <hanyu2@asu.edu>
2016-07-06 11:54:41 -0700

    Add link to app in recomm_item

--- appstore/client/components/recomm_app_item.js
+++ appstore/client/components/recomm_app_item.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Apps } from '/imports/collections/apps';
+import {Link} from 'react-router'
 
 const RecommAppItem = React.createClass({
   mixins: [ReactMeteorData],
@@ -16,16 +17,23 @@ const RecommAppItem = React.createClass({
   },
 
   getContent() {
+    var app_id = this.props.app_id;
+    const path = `/app/${app_id}`
     return (
       <li className="media list-group-item">
         <div>
           <div className="media-left">
+          <Link to={path}>
             <img src={this.data.app[0].icon_url} />
+          </Link>
           </div>
+        
           <div className="media-body">
             <a>
               <font className="media-heading">
+              <Link to={path}>
                 {this.data.app[0].app_name}
+              </Link>
               </font>
             </a>
           </div>
